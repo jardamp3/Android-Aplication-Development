9b1203fc69a97e591d2465912b3b5f1d7c62c7d2
JerryX <jerryx@darkruby.net>
2017-09-10 16:58:53 +0300

    Import LaunchMapExercise

---
+++ week_36/LaunchMapExercise/app/src/main/java/fi/jamk/launchmapexercise/MainActivity.java
@@ -0,0 +1,33 @@
+package fi.jamk.launchmapexercise;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showMap(View view) {
+        // get lat and lng values
+        EditText editText1 = (EditText) findViewById(R.id.lat);
+        EditText editText2 = (EditText) findViewById(R.id.lng);
+        String numberOne = editText1.getText().toString();
+        String numberTwo = editText2.getText().toString();
+        double lat = Double.parseDouble(numberOne);
+        double lng = Double.parseDouble(numberTwo);
+        // show map
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("geo:"+lat+","+lng));
+        startActivity(intent);
+    }
+}
